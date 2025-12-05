e8185ff77936f48b37c11e071636d753dad70cef
MAYA-P-code <mayaaboamer25@gmail.com>
2025-12-05 21:04:56 +0200

    Professor class with sql query
    Refactor view_all_registered_students method to improve SQL query and class constructor.

--- srs/models/professor.py
+++ srs/models/professor.py
@@ -1,23 +1,29 @@
 from app import get_db
 
 class Professor:
-    def __init__(self, pID: str, pname: str, password: str):
+    def _init_(self, pID: str, pname: str, password: str):
         self.pID = pID       
         self.pname = pname   
         self.password = password  
 
-  def view_all_registered_students(self, course_id: str):
+    def view_all_registered_students(self):
+
         db = get_db()
 
-        rows = db.execute(
-            """SELECT s.stID, s.sname, r.grade
-             FROM Registered_In AS r
-             JOIN Student      AS s ON r.stuID = s.stID
-             JOIN Course       AS c ON r.coID = c.cID
-            WHERE c.cID = ? AND c.PrID = ?;
-            """ ,
-            (course_id, self.pID),
-        ).fetchall()
+        sql = """SELECT s.stID, s.sname
+                 FROM Student s
+                 JOIN Registered_In r ON s.stID = r.stuID
+                 JOIN Course c        ON r.coID = c.cID
+                 WHERE c.PrID = ? 
+                 GROUP BY s.stID, s.sname
+                 HAVING COUNT(c.cID) = (
+                     SELECT COUNT(*) 
+                     FROM Course 
+                     WHERE PrID = ? 
+                 );
+              """
+
+        rows = db.execute(sql,(self.pID, self.pID)).fetchall()
 
         students = [dict(row) for row in rows]
 
