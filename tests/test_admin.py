d99a16a073aff2726ef1fb1d9c12843b2848f974
HazemRDWN <s-hazem.refaat@zewailcity.edu.eg>
2025-12-18 01:12:27 +0200

    finished user adding tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -6,7 +6,7 @@ def test_admin_add_student(mock_repo_class, client):
     
     mock_instance = MagicMock()
     mock_repo_class.return_value = mock_instance
-    mock_instance.add_student.return_value = "Successfully added Hazem"
+    mock_instance.add_student.return_value = "Successfully added Ahmed"
 
     with client.session_transaction() as sess:
         sess['userID'] = 1
@@ -15,10 +15,48 @@ def test_admin_add_student(mock_repo_class, client):
     response = client.post("/AddUser", data={
         "usertype": "Student",
         "id": "202401389",
-        "name": "Hazem",
+        "name": "Ahmed",
         "password": "123"
     })
 
     assert response.status_code == 200
 
-#add other users with admin
+@patch("srs.controllers.admin_controller.adminRepo")
+def test_admin_add_prof(mock_repo_class, client):
+    
+    mock_instance = MagicMock()
+    mock_repo_class.return_value = mock_instance
+    mock_instance.add_prof.return_value = "Successfully added Mohammed"
+
+    with client.session_transaction() as sess:
+        sess['userID'] = 1
+        sess['type'] = 'Admin'
+
+    response = client.post("/AddUser", data={
+        "usertype": "Professor",
+        "id": "20240000",
+        "name": "Mohammed",
+        "password": "prof123"
+    })
+
+    assert response.status_code == 200
+
+@patch("srs.controllers.admin_controller.adminRepo")
+def test_admin_add_admin(mock_repo_class, client):
+    
+    mock_instance = MagicMock()
+    mock_repo_class.return_value = mock_instance
+    mock_instance.add_admin.return_value = "Successfully added George"
+
+    with client.session_transaction() as sess:
+        sess['userID'] = 1
+        sess['type'] = 'Admin'
+
+    response = client.post("/AddUser", data={
+        "usertype": "Admin",
+        "id": "20240000",
+        "name": "George",
+        "password": "admin123"
+    })
+
+    assert response.status_code == 200
