c02115773d6d7ab3c1c46c6884b6924ef97e2a70
MAYA-P-code <mayaaboamer25@gmail.com>
2025-12-19 12:11:03 +0200

    manage courses in admin_controller.py

--- srs/controllers/admin_controller.py
+++ srs/controllers/admin_controller.py
@@ -35,3 +35,34 @@ def addUser():
         status = adminRepo(db, admin_id).add_admin(id, name, password)
 
     return render_template("ManageUsers.html", message = status)
+
+@admin_bp.route('/ManageCourses')
+def manage_courses():
+    return render_template("ManageCourses.html")
+
+
+@admin_bp.route("/AddCourse", methods=["POST"])
+def addCourse():
+    c_id = request.form["course_id"]
+    cname = request.form["course_name"]
+    capacity = request.form["capacity"]
+    professor_id = request.form["professor_id"]
+
+    db = get_db()
+    admin_id = session.get('userID')
+
+    status = adminRepo(db, admin_id).add_course(c_id, cname, capacity, professor_id)
+
+    return render_template("ManageCourses.html", message=status)
+
+
+@admin_bp.route("/RemoveCourse", methods=["POST"])
+def removeCourse():
+    c_id = request.form["course_id"]
+
+    db = get_db()
+    admin_id = session.get('userID')
+
+    status = adminRepo(db, admin_id).remove_course(c_id)
+
+    return render_template("ManageCourses.html", message=status)
